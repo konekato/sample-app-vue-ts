d4df10f60b11fba8dc5c8d5b532157a118cd54cb
konekato <k1shock1125@gmail.com>
2023-02-26 16:29:22 +0900

    Initial commit

---
+++ src/pages/PostsPageX.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="row">
+    <posts-component
+    ></posts-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import PostsComponent from 'src/components/PostsComponent.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PostPage',
+  components: { PostsComponent },
+  setup () {
+    return
+  }
+})
+</script>

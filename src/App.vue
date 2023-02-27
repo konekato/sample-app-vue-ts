b2c0a68e6ffbc719bc9cd9cc77e345c465a0083d
konekato <k1shock1125@gmail.com>
2023-02-27 15:01:02 +0900

    Add: 404ページ用のストアなど追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <router-view />
+  <error-not-found v-if="isNotFound"></error-not-found>
+  <router-view v-else />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, toRefs } from 'vue';
+import ErrorNotFound from './pages/ErrorNotFound.vue';
+import { useErrorStore } from 'src/stores/error';
 
 export default defineComponent({
-  name: 'App'
+    name: 'App',
+    components: { ErrorNotFound },
+    setup () {
+      const { isNotFound } = toRefs(useErrorStore());
+      return { isNotFound };
+    }
 });
 </script>
